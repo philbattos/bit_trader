d5d2f2a7c68c335f590da92d88c9e8e153facac5
Phil Battos <philbattos@gmail.com>
2016-10-09 23:14:22 -0600

    Add files to .gitignore

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161007222223) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "accounts", force: :cascade do |t|
     t.string   "gdax_id"
     t.string   "currency"
