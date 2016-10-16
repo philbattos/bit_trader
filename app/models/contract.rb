37e09f0809fffb3846c6f45435de8679a3d31521
Phil Battos <philbattos@gmail.com>
2016-10-16 00:02:45 -0600

    Refactor rate limit message logic

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -24,8 +24,9 @@ class Contract < ActiveRecord::Base
 
   def self.match_open_buys
     current_ask = Market.current_ask
+    return missing_price('ask') if current_ask == 0.0
+
     with_buy_without_sell.each do |contract|
-      next if current_ask == 0.0
       min_sell_price = contract.buy_order.price + PROFIT
       sell_price     = [current_ask, min_sell_price].compact.max.round(7)
       sell_order     = Order.place_sell(sell_price)
@@ -40,8 +41,9 @@ class Contract < ActiveRecord::Base
 
   def self.match_open_sells
     current_bid = Market.current_bid
+    return missing_price('bid') if current_bid == 0.0
+
     with_sell_without_buy.each do |contract|
-      next if current_ask == 0.0
       max_buy_price = contract.sell_order.price - PROFIT
       buy_price     = [current_bid, max_buy_price].min.round(7)
       buy_order     = Order.place_buy(buy_price)
@@ -56,11 +58,7 @@ class Contract < ActiveRecord::Base
 
   def self.place_new_buy_order
     # a new BUY order gets executed when the USD account has enough funds to buy the selected amount
-    if my_buy_price == 0.0
-      puts "Buy price $0... Could be rate limit error"
-      return
-    end
-
+    return missing_price('buy') if my_buy_price == 0.0
     new_order = Order.place_buy(my_buy_price)
 
     if new_order
@@ -73,11 +71,7 @@ class Contract < ActiveRecord::Base
 
   def self.place_new_sell_order
     # a new SELL order gets executed when the BTC account has enough funds to sell the selected amount
-    if my_ask_price == 0.0
-      puts "Ask price $0... Could be rate limit error"
-      return
-    end
-
+    return missing_price('sell') if my_ask_price == 0.0
     new_order = Order.place_sell(my_ask_price)
 
     if new_order
@@ -124,6 +118,10 @@ class Contract < ActiveRecord::Base
 
   def orders
     [ buy_order, sell_order ]
+  end
+
+  def self.missing_price(type)
+    "GDAX orderbook returned a #{type} price of $0... It could be the result of a rate limit error."
   end
 
 end
