45e18f0946baa73262677eb4de259855f02dfa50
Phil Battos <philbattos@gmail.com>
2017-01-08 03:27:49 -0700

    Fix ROI on chart

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -30,18 +30,20 @@ class OrdersController < ApplicationController
 
     @chart2 = LazyHighCharts::HighChart.new('graph') do |f|
       f.title(text: "Contracts")
-      f.xAxis(type: "datetime", categories: @resolved_contracts.count.keys)
+      f.xAxis(categories: @resolved_contracts.count.keys)
+      f.yAxis(type: "datetime", categories: @resolved_contracts.count.keys)
       f.labels(items: [html:"Contracts Metrics", style: {left: "40px", top: "8px", color: "black"}])
       f.series(type: 'column', name: 'Total Contracts', yAxis: 0, data: @resolved_contracts.count.values)
-      f.series(type: 'column', name: 'ROI', yAxis: 1, data: @resolved_contracts.sum(:roi).values)
+      f.series(type: 'column', name: 'ROI', yAxis: 1, data: @resolved_contracts.sum(:roi).values.map {|c| c.to_f})
       # f.series(:type=> 'column', :name=> 'John',:data=> [2, 3, 5, 7, 6])
       # f.series(:type=> 'column', :name=> 'Joe',:data=> [4, 3, 3, 9, 0])
+
       f.yAxis [
         {title: {text: "Total Contracts", margin: 70} },
         {title: {text: "ROI"}, opposite: true},
       ]
 
-      f.series(:type=> 'spline', :name=> 'Resolved Contracts', :data=> @resolved_contracts.count.to_a)
+      f.series(type: 'spline', name: 'Resolved Contracts', data: @resolved_contracts.count.to_a)
 
       f.legend(align: 'right', verticalAlign: 'top', y: 75, x: -50, layout: 'vertical')
       # f.chart({defaultSeriesType: "column"})
