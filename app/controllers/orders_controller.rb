025b7b9e5e57640124a0d5c914d7bfef7895812b
Phil Battos <philbattos@gmail.com>
2017-01-08 17:04:44 -0700

    Format dates on ROI chart

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -26,15 +26,15 @@ class OrdersController < ApplicationController
     end
 
     @unresolved_contracts = Contract.unresolved
-    @resolved_contracts_daily = Contract.order("date_trunc('day', updated_at)").group("date_trunc('day', updated_at)")
+    @resolved_contracts_daily = Contract.resolved.order("date_trunc('day', updated_at)").group("date_trunc('day', updated_at)")
 
     @chart2 = LazyHighCharts::HighChart.new('graph') do |f|
       f.title(text: "Contracts")
-      f.xAxis(categories: @resolved_contracts_daily.count.keys)
-      f.yAxis(type: "datetime", categories: @resolved_contracts_daily.count.keys)
-      f.labels(items: [html:"Contracts Metrics", style: {left: "40px", top: "8px", color: "black"}])
+      f.xAxis(categories: @resolved_contracts_daily.count.keys.map {|c| c.in_time_zone("Mountain Time (US & Canada)").strftime("%-m/%d").strip })
+      f.yAxis(type: "datetime", categories: @resolved_contracts_daily.count.keys.map {|c| c.in_time_zone("Mountain Time (US & Canada)").strftime("%-m/%y").strip })
+      f.labels(items: [html:"Contracts (Daily)", style: {left: "40px", top: "8px", color: "black"}])
       f.series(type: 'column', name: 'Total Contracts', yAxis: 0, data: @resolved_contracts_daily.count.values)
-      f.series(type: 'column', name: 'ROI', yAxis: 1, data: @resolved_contracts_daily.sum(:roi).values.map {|c| c.to_f})
+      f.series(type: 'column', name: 'ROI', yAxis: 1, data: @resolved_contracts_daily.sum(:roi).values.map(&:to_f))
       # f.series(:type=> 'column', :name=> 'John',:data=> [2, 3, 5, 7, 6])
       # f.series(:type=> 'column', :name=> 'Joe',:data=> [4, 3, 3, 9, 0])
 
@@ -49,14 +49,14 @@ class OrdersController < ApplicationController
       # f.chart({defaultSeriesType: "column"})
     end
 
-    @resolved_contracts_hourly = Contract.order("date_trunc('hour', updated_at)").group("date_trunc('hour', updated_at)")
+    @resolved_contracts_hourly = Contract.resolved.order("date_trunc('hour', updated_at)").group("date_trunc('hour', updated_at)")
 
     @chart3 = LazyHighCharts::HighChart.new('graph') do |f|
       f.title(text: "Contracts Profit")
       f.xAxis(categories: @resolved_contracts_hourly.count.keys.last(30).map {|c| c.in_time_zone("Mountain Time (US & Canada)").strftime("%l%P").strip })
       f.yAxis(type: "datetime", categories: @resolved_contracts_hourly.count.keys.last(30).map {|c| c.in_time_zone("Mountain Time (US & Canada)").strftime("%_m/%d").strip })
-      # f.labels(items: [html:"Contracts Metrics", style: {left: "40px", top: "8px", color: "black"}])
-      # f.series(type: 'column', name: 'Total Contracts', yAxis: 0, data: @resolved_contracts_hourly.count.values)
+      f.labels(items: [html:"Contracts (Hourly)", style: {left: "40px", top: "8px", color: "black"}])
+      f.series(type: 'column', name: 'Completed Contracts', yAxis: 0, data: @resolved_contracts_hourly.count.values)
       f.series(type: 'column', name: 'ROI', yAxis: 1, data: @resolved_contracts_hourly.sum(:roi).values.last(30).map(&:to_f))
       # f.series(:type=> 'column', :name=> 'John',:data=> [2, 3, 5, 7, 6])
       # f.series(:type=> 'column', :name=> 'Joe',:data=> [4, 3, 3, 9, 0])
