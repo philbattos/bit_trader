61c89e80b7266095edaab5e817de55f317b28c57
Phil Battos <philbattos@gmail.com>
2017-01-11 02:04:37 -0700

    Fix contracts chart

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -93,8 +93,7 @@ class OrdersController < ApplicationController
       f.xAxis(
         title: { text: "Date", margin: 30 },
         type: "datetime",
-        # categories: @unresolved_contracts
-        # categories: @unresolved_contracts.order("date_trunc('day', created_at)").map {|c| c.created_at.in_time_zone("Mountain Time (US & Canada)").strftime("%_m/%d").strip }.uniq
+        categories: @unresolved_contracts.order("date_trunc('day', created_at)").map {|c| c.created_at.in_time_zone("Mountain Time (US & Canada)").strftime("%_m/%d").strip }.uniq
       )
 
       f.yAxis(
