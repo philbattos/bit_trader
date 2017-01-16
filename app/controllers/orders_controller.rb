c7c4da2267d7b54625bb9304fbbc18ddf0e80e16
Phil Battos <philbattos@gmail.com>
2017-01-16 02:14:47 -0700

    Fix chart dates

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -115,7 +115,7 @@ class OrdersController < ApplicationController
             pointInterval: 24 * 3600 * 1000 # one day
           },
           tooltip: {
-            borderWidth: 3
+            borderWidth: 3,
             headerFormat: '<b>{series.name}</b><br>',
             pointFormat: '${point.x}, { DateTime.strptime(point.y, "%s").in_time_zone("Mountain Time (US & Canada)").strftime("%_m/%d %l:%M%P").strip }'
             # pointFormat: '{Time.at(point.x).in_time_zone("Mountain Time (US & Canada)").strftime("%_m/%d %l:%M%P").strip}, {point.y}'
