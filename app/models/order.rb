fe2ba4cf310f77005c169ad7d4f3f8d8a8c55a13
Phil Battos <philbattos@gmail.com>
2017-04-29 00:15:41 -0600

    Prevent NaN from being saved to orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -230,7 +230,6 @@ class Order < ActiveRecord::Base
       response = check_status(order.gdax_id)
       if response && response.status != order.gdax_status
         puts "Updating status of #{order.type} #{order.id} from #{order.gdax_status} to #{response.status}"
-        puts "response: #{response.inspect}"
         # NOTE: Coinbase-exchange gem automatically converts numeric response values into decimals
         order.update(
           gdax_status:         response.status,
@@ -259,9 +258,7 @@ class Order < ActiveRecord::Base
   end
 
   def self.calculate_filled_price(response)
-    puts "response.executed_value: #{response.executed_value.inspect}"
-    puts "response.filled_size: #{response.filled_size.inspect}"
-    return nil if response.executed_value.nil? || response.filled_size.nil?
+    return nil if response.executed_value.nil? || response.filled_size.nil? || response.filled_size.zero?
     response.executed_value / response.filled_size
   end
 
