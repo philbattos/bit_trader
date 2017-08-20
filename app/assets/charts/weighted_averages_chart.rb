5c890de68caeec9797502c13da3f98a1387c01dd
Phil Battos <philbattos@gmail.com>
2017-08-19 22:41:15 -0600

    Update chart

--- app/assets/charts/weighted_averages_chart.rb
+++ app/assets/charts/weighted_averages_chart.rb
@@ -17,64 +17,64 @@ module Charts
 
         f.series(
           name: 'Bitcoin Price',
-          data: Metric.with_averages.where("id > ?", 21654).order(:id).pluck(:created_at, :bitcoin_price).map {|m| [m.first.to_i * 1000, m.last.to_f.round(2)] },
+          data: Metric.with_averages.where("id > ?", 21690).order(:id).pluck(:created_at, :bitcoin_price).map {|m| [m.first.to_i * 1000, m.last.to_f.round(2)] },
           yAxis: 0,
           lineWidth: 3
         )
 
         f.series(
           name: '30-Minute Average',
-          data: Metric.with_averages.where("id > ?", 21654).order(:id).pluck(:created_at, :average_weighted_30_minute).map {|m| [m.first.to_i * 1000, m.last.to_f.round(2)] },
+          data: Metric.with_averages.where("id > ?", 21690).order(:id).pluck(:created_at, :average_weighted_30_minute).map {|m| [m.first.to_i * 1000, m.last.to_f.round(2)] },
           yAxis: 0
         )
 
         f.series(
           name: '1-Hour Average',
-          data: Metric.with_averages.where("id > ?", 21654).order(:id).pluck(:created_at, :average_weighted_1_hour).map {|m| [m.first.to_i * 1000, m.last.to_f.round(2)] },
+          data: Metric.with_averages.where("id > ?", 21690).order(:id).pluck(:created_at, :average_weighted_1_hour).map {|m| [m.first.to_i * 1000, m.last.to_f.round(2)] },
           yAxis: 0
         )
 
         f.series(
           name: '4-Hour Average',
-          data: Metric.with_averages.where("id > ?", 21654).order(:id).pluck(:created_at, :average_weighted_4_hour).map {|m| [m.first.to_i * 1000, m.last.to_f.round(2)] },
+          data: Metric.with_averages.where("id > ?", 21690).order(:id).pluck(:created_at, :average_weighted_4_hour).map {|m| [m.first.to_i * 1000, m.last.to_f.round(2)] },
           yAxis: 0
         )
 
         f.series(
           name: '6-Hour Average',
-          data: Metric.with_averages.where("id > ?", 21654).order(:id).pluck(:created_at, :average_weighted_6_hour).map {|m| [m.first.to_i * 1000, m.last.to_f.round(2)] },
+          data: Metric.with_averages.where("id > ?", 21690).order(:id).pluck(:created_at, :average_weighted_6_hour).map {|m| [m.first.to_i * 1000, m.last.to_f.round(2)] },
           yAxis: 0
         )
 
         f.series(
           name: '10-Hour Average',
-          data: Metric.with_averages.where("id > ?", 21654).order(:id).pluck(:created_at, :average_weighted_10_hour).map {|m| [m.first.to_i * 1000, m.last.to_f.round(2)] },
+          data: Metric.with_averages.where("id > ?", 21690).order(:id).pluck(:created_at, :average_weighted_10_hour).map {|m| [m.first.to_i * 1000, m.last.to_f.round(2)] },
           yAxis: 0
         )
 
         f.series(
           type: 'spline', # remove?
           name: '13-Hour Average',
-          data: Metric.with_averages.where("id > ?", 21654).order(:id).pluck(:created_at, :average_weighted_13_hour).map {|m| [m.first.to_i * 1000, m.last.to_f.round(2)] },
+          data: Metric.with_averages.where("id > ?", 21690).order(:id).pluck(:created_at, :average_weighted_13_hour).map {|m| [m.first.to_i * 1000, m.last.to_f.round(2)] },
           yAxis: 0
         )
 
         f.series(
           name: '21-Hour Average',
-          data: Metric.with_averages.where("id > ?", 21654).order(:id).pluck(:created_at, :average_weighted_21_hour).map {|m| [m.first.to_i * 1000, m.last.to_f.round(2)] },
+          data: Metric.with_averages.where("id > ?", 21690).order(:id).pluck(:created_at, :average_weighted_21_hour).map {|m| [m.first.to_i * 1000, m.last.to_f.round(2)] },
           yAxis: 0
         )
 
         f.series(
           name: '25-Hour Average',
-          data: Metric.with_averages.where("id > ?", 21654).order(:id).pluck(:created_at, :average_weighted_25_hour).map {|m| [m.first.to_i * 1000, m.last.to_f.round(2)] },
+          data: Metric.with_averages.where("id > ?", 21690).order(:id).pluck(:created_at, :average_weighted_25_hour).map {|m| [m.first.to_i * 1000, m.last.to_f.round(2)] },
           yAxis: 0
         )
 
         f.series(
           type: 'spline', # remove?
           name: '43-Hour Average',
-          data: Metric.with_averages.where("id > ?", 21654).order(:id).pluck(:created_at, :average_weighted_43_hour).map {|m| [m.first.to_i * 1000, m.last.to_f.round(2)] },
+          data: Metric.with_averages.where("id > ?", 21690).order(:id).pluck(:created_at, :average_weighted_43_hour).map {|m| [m.first.to_i * 1000, m.last.to_f.round(2)] },
           yAxis: 0
         )
 
