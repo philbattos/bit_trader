5d40c0192555445244a67c51642b59d42e9572f0
Phil Battos <phil.battos@oracle.com>
2017-08-22 14:20:08 -0600

    Re-order charts

--- app/controllers/traders_controller.rb
+++ app/controllers/traders_controller.rb
@@ -21,15 +21,15 @@ class TradersController < ApplicationController
 
     # @chart1a = Charts::OrdersChart.build_chart
     # @chart1b = Charts::ContractsChart.build_chart
-    @chart2 = Charts::AccountGrowthChart.build_chart
-    @chart3 = Charts::ContractsProfitChart.build_chart
     # @chart4 = Charts::OpenContractsChart.build_chart
-    @chart5 = Charts::UnresolvedContractsChart.build_chart
+    # @chart5 = Charts::UnresolvedContractsChart.build_chart
     # @chart6 = Charts::AccountValueChart.build_chart
     # @chart7 = Charts::MovingAveragesChart.build_chart
     @chart8 = Charts::WeightedAveragesChart.build_chart
     @chart9 = Charts::TrendlineContractsChart.build_chart
     @chart10 = Charts::TrendlineROIChart.build_chart
+    @chart3 = Charts::ContractsProfitChart.build_chart
+    @chart2 = Charts::AccountGrowthChart.build_chart
 
     @chart_settings = Charts::GlobalSettings.build
 
