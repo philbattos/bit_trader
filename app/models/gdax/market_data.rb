3266c60addd03079504872a39f10b21e35806ed9
Phil Battos <phil.battos@oracle.com>
2017-09-23 23:49:21 -0600

    Add buffer when comparing current price to recent floor/ceiling

--- app/models/gdax/market_data.rb
+++ app/models/gdax/market_data.rb
@@ -18,11 +18,11 @@ module GDAX
     end
 
     def self.new_high_price?(time)
-      last_saved_trade.price >= trades_since(time).maximum(:price)
+      last_saved_trade.price >= (trades_since(time).maximum(:price) * 0.997)
     end
 
     def self.new_low_price?(time)
-      last_saved_trade.price <= trades_since(time).minimum(:price)
+      last_saved_trade.price <= (trades_since(time).minimum(:price) * 1.003)
     end
 
     def self.last_saved_trade
